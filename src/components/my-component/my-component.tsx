5e1bf9d846a147fffbdcd5d053e5fc465e8210c7
Leticia Campos <leticia.silva105@fatec.sp.gov.br>
2022-08-02 18:09:19 -0300

    feat: componente padrão

--- src/components/my-component/my-component.tsx
+++ src/components/my-component/my-component.tsx
@@ -1,32 +1,14 @@
-import { Component, Prop, h } from '@stencil/core';
-import { format } from '../../utils/utils';
-
+import { Component, Prop, h} from '@stencil/core';
 @Component({
   tag: 'my-component',
   styleUrl: 'my-component.css',
   shadow: true,
 })
 export class MyComponent {
-  /**
-   * The first name
-   */
-  @Prop() first: string;
 
-  /**
-   * The middle name
-   */
-  @Prop() middle: string;
-
-  /**
-   * The last name
-   */
-  @Prop() last: string;
-
-  private getText(): string {
-    return format(this.first, this.middle, this.last);
-  }
+  @Prop() theme: 'green' | 'red' = 'green';
 
   render() {
-    return <div>Hello, World! I'm {this.getText()}</div>;
+    return <div class={this.theme}>Hello, World!</div>;
   }
 }
